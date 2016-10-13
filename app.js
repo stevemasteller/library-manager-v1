d3a3f924ab6411fb92b0088700dee5272818a81e
stevemasteller <stevermasteller@gmail.com>
2016-10-12 22:46:59 -0400

    Removed some unused code

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express      = require('express');
 var path         = require('path');
 var favicon      = require('serve-favicon');
-var logger       = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 
 var books        = require('./routes/books');
@@ -16,10 +14,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // vendor scripts
@@ -33,37 +29,5 @@ app.get('/vendor/angular-route.js', function(req, res) {
 app.use('/api/books', books);
 app.use('/api/loans', loans);
 app.use('/api/patrons', patrons);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
